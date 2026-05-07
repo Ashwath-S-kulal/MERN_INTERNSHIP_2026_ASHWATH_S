e5867d19f643cab0cdfb7623b3258dbca33244a6
Ashwath-S-kulal <ashwathkulal2004@gmail.com>
2026-05-07 09:35:47 +0530

    small changes

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,61 @@
+// import express from "express";
+// import dotenv from "dotenv";
+// import connectDB from "./config/db.js";
+// import userRoute from "./routes/userRoute.js";
+// import adminRoute from "./routes/adminRoute.js"
+// import serviceProviderRoute  from "./routes/serviceproviderRoute.js"
+// import bookingRoutes from "./routes/bookingRoutes.js"
+// import MemberRoutes from "./routes/memberRoute.js"
+// import reviewRoutes from "./routes/reviewRoutes.js"
+
+// import cors from "cors";
+// dotenv.config();
+
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+// app.use(express.json());
+
+// app.use(cors({
+//   origin: process.env.FRONTEND_URI,
+//   credentials: true,
+// }
+// ));
+
+// app.get("/", (req, res) => {
+//   res.send("Backend running");
+// });
+
+// app.use("/api/user", userRoute);
+// app.use("/api/admin", adminRoute);
+// app.use("/api/serviceprovider", serviceProviderRoute);
+// app.use("/api/booking", bookingRoutes);
+// app.use("/api/member", MemberRoutes);
+// app.use('/api/reviews', reviewRoutes);
+
+// app.listen(PORT, () => {
+//   connectDB();
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+
+
+
+
+
 
 
 
 
 import express from "express";
- import dotenv from "dotenv";
+import dotenv from "dotenv";
 import connectDB from "./config/db.js";
- import userRoute from "./routes/userRoute.js";
+import userRoute from "./routes/userRoute.js";
 import adminRoute from "./routes/adminRoute.js";
- import serviceProviderRoute from "./routes/serviceproviderRoute.js";
- import bookingRoutes from "./routes/bookingRoutes.js";
- import MemberRoutes from "./routes/memberRoute.js";
+import serviceProviderRoute from "./routes/serviceproviderRoute.js";
+import bookingRoutes from "./routes/bookingRoutes.js";
+import MemberRoutes from "./routes/memberRoute.js";
 import reviewRoutes from "./routes/reviewRoutes.js";
- import cors from "cors";
+import cors from "cors";
 
 dotenv.config();
 
@@ -21,14 +65,16 @@ connectDB();
 
 app.use(express.json());
 
-app.use(cors({
-origin: process.env.FRONTEND_URI,
- credentials: true,
- }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URI,
+    credentials: true,
+  }),
+);
 
 app.get("/", (req, res) => {
- res.send("Backend is running");
- });
+  res.send("Backend is running");
+});
 
 app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
@@ -36,6 +82,5 @@ app.use("/api/serviceprovider", serviceProviderRoute);
 app.use("/api/booking", bookingRoutes);
 app.use("/api/member", MemberRoutes);
 app.use("/api/reviews", reviewRoutes);
-
 
 export default app;
