f8ad9ac01a507afd72fd792ff665e065da752a53
Ashwath-S-kulal <ashwathkulal2004@gmail.com>
2026-06-25 09:17:38 +0530

    logo

--- backend/server.js
+++ backend/server.js
@@ -46,8 +46,6 @@
 
 
 
-
-
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
--- frontend/src/components/GenerateDesc.jsx
+++ frontend/src/components/GenerateDesc.jsx
@@ -32,6 +32,7 @@ export default function AiDescriptionInput({
         }
     };
 
+
     return (
         <button
             type="button"
